09d487b759eb19687bdca72b4844b1ce0842f169
Victor Loyinmi (val449) <val449@mail.usask.ca>
2017-11-30 16:59:16 -0600

    Just some updates

--- uShovel/LoginViewController.swift
+++ uShovel/LoginViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    private var authURL:URL = URL(string: "https://cmpt370-2017.herokuapp.com/authenticate")!
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        let email=emailField.text!
+        let pass=passField.text!
+        
+        authenticate(email:email, pass:pass)
+        
+    }
+    func authenticate(email: String, pass:String){
+        let parameters=["email":"\(email)","password":"\(pass)"]
+        var request=URLRequest(url: authURL)
+        request.httpMethod = "POST"
+        request.httpBody=try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        URLSession.shared.dataTask(with: authURL){(data,response,error)in
+            if let response = response{
+                print (response)
+            }
+            if let data=data{
+                print (data)
+                do{
+                    let postJson=try JSONSerialization.jsonObject(with: data, options: [])
+                    print(postJson)
+                }
+                catch{
+                    print(error.localizedDescription)
+                }
+            }
+            }.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- uShovel/SignUpViewController.swift
+++ uShovel/SignUpViewController.swift
@@ -61,6 +61,7 @@ class SignUpViewController: UIViewController {
     
     }
     
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
